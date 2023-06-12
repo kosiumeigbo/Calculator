cbfd8bd98537e0181f8360b22a8d50173de6a68c
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2023-06-12 20:08:12 +0100

    Edited the class names to make it easier for them to be used in the Javascript

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 "use strict";
 
 const screen = document.querySelector(".screen");
-screen.textContent = "123";
+const buttonsArea = document.querySelector(".buttons");
+const selectedBackgroundColor = "#fee48f";
+const selectedBorderColor = "#b18907";
+// screen.textContent = "";
+
+let numberString = ""; // To be updated as user presses the digit keypads. Maximum will be
+let workingNumber; // To be updated when the user is done entering the number
+let decimalInNumber = false; // To be updated when decimal number has been inputed
+let totalNumberCounter = 0; // To make sure that the number has no greater than 9 digits
+let workingNumbersArray = []; // To compile working numbers
+
+buttonsArea.addEventListener("click", function (e) {
+  // If target is a number
+  if (e.target.classList.contains("button-number")) {
+    if (numberString.length < 9) numberString += e.target.textContent;
+    console.log(numberString);
+    console.log(Number(numberString));
+    screen.textContent = numberString;
+  }
+});
