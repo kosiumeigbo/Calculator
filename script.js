e5d1d725f6f793a8f3738315387d0dc95a9f0e79
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2023-06-13 21:53:35 +0100

    Updated JavaScript logic for entering numbers from the index state

--- script.js
+++ script.js
@@ -2,24 +2,59 @@
 
 const screen = document.querySelector(".screen");
 const buttonsArea = document.querySelector(".buttons");
+const buttonClear = document.querySelector(".button-clear");
 const selectedBackgroundColor = "#fee48f";
 const selectedBorderColor = "#b18907";
 // screen.textContent = "";
 
-let numberString = ""; // To be updated as user presses the digit keypads. Maximum will be
-let workingNumber; // To be updated when the user is done entering the number
+let workingNumberString = "0"; // To be updated when the user is done entering the number
+let workingNumberStringDigitCounter = 0; // To make sure that the entered number has no greater than 9 digits in total
 let decimalInNumber = false; // To be updated when decimal number has been inputed
-let totalNumberCounter = 0; // To make sure that the number has no greater than 9 digits
 let workingNumbersArray = []; // To compile working numbers
 
-buttonsArea.addEventListener("click", function (e) {
-  // If target is a number
-  if (e.target.classList.contains("number")) {
-    if (numberString.length < 9) numberString += e.target.textContent;
-    console.log(numberString);
-    console.log(Number(numberString));
-    screen.textContent = numberString;
-  }
+const calculator = function (e) {
+  const pressedButton = e.target; // Registering the pressed button
 
-  // If target is the decimal button
-});
+  // If pressed button is a number
+  if (pressedButton.classList.contains("number")) {
+    // Change the CE button to C
+    buttonClear.textContent = "C";
+
+    // If pressed number is a 0 and the working number is 0
+    if (
+      pressedButton.dataset.number === "0" &&
+      workingNumberString.length === 1 &&
+      workingNumberString.includes("0")
+    )
+      return;
+    // If pressed number is not a 0 but the working number is 0
+    else if (
+      workingNumberString.length === 1 &&
+      workingNumberString.includes("0") &&
+      pressedButton.dataset.number !== "0"
+    ) {
+      workingNumberString = pressedButton.dataset.number;
+      workingNumberStringDigitCounter++;
+      screen.textContent = workingNumberString;
+    }
+    // If working number is a single digit but not 0
+    else if (
+      workingNumberString.length === 1 &&
+      !workingNumberString.includes("0")
+    ) {
+      workingNumberString += pressedButton.dataset.number;
+      workingNumberStringDigitCounter++;
+      screen.textContent = workingNumberString;
+    }
+    // The maximum number of digits a number should have is 9 digits
+    else if (workingNumberStringDigitCounter < 9) {
+      workingNumberString += pressedButton.dataset.number;
+      workingNumberStringDigitCounter++;
+      screen.textContent = workingNumberString;
+    } else {
+      return;
+    }
+  }
+};
+
+buttonsArea.addEventListener("click", calculator);
