821943422ede1ac47374bdbdb3c884860bd07b0e
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2023-06-14 20:17:27 +0100

    Updated script.js file with more operations
    Added in the multiplication and division logic to the Javascript file

--- script.js
+++ script.js
@@ -10,8 +10,7 @@ const selectedBorderColor = "#b18907";
 let workingNumberString = "0"; // To be updated when the user is done entering the number
 let workingNumberStringDigitCounter = 0; // To make sure that the entered number has no greater than 9 digits in total
 let decimalPointActivated = false; // To be updated when decimal point has been input
-let operationActivated = false;
-// let workingNumbersArray = []; // To compile working numbers
+let operationActivated = false; // To be updated to true when operation button has been pressed
 let additionCounter = 0; // To be updated when addition button is pressed
 let subtractionCounter = 0; // To be updated when subtraction button is pressed
 let divisionCounter = 0; // To be updated when division button is pressed
@@ -20,8 +19,8 @@ let savedWorkingNumber; // Number to be updated as one of the 4 main operations 
 let previousEnteredNumber; // Last number to be entered
 
 // Function to update working number string by adding characters to it
-const updateWorkingNumberString = function (adder1, adder2) {
-  workingNumberString = adder1 + adder2;
+const updateWorkingNumberString = function (rootString, adder) {
+  workingNumberString = rootString + adder;
 };
 
 // Function to update the number string digit counter
@@ -35,8 +34,8 @@ const renderScreen = function (number) {
 };
 
 // Function combining updateWorkingNumberString and updateWorkingNumberStringDigitCounter
-const numberPressed = function (adder1, adder2) {
-  updateWorkingNumberString(adder1, adder2);
+const numberPressed = function (rootString, adder) {
+  updateWorkingNumberString(rootString, adder);
   updateWorkingNumberStringDigitCounter();
 };
 
@@ -54,7 +53,10 @@ const calculator = function (e) {
       workingNumberString.length === 1 &&
       workingNumberString.includes("0")
     ) {
-      return;
+      // If an operation is currently active;
+      if (operationActivated) {
+        renderScreen(workingNumberString);
+      }
     }
     // If pressed number is not a 0 but the working number is 0
     else if (
@@ -110,7 +112,7 @@ const calculator = function (e) {
       additionCounter++;
       operationActivated = true;
     }
-    // If addition button has been pressed already
+    // If addition button has been pressed already in this round of calculations
     else {
       savedWorkingNumber += Number(workingNumberString);
       previousEnteredNumber = Number(workingNumberString);
@@ -118,6 +120,84 @@ const calculator = function (e) {
       workingNumberString = "0";
       additionCounter++;
       renderScreen(savedWorkingNumber);
+    }
+  }
+
+  // If pressed button is subtraction button
+  if (pressedButton.classList.contains("subtraction")) {
+    // If subtraction hasn't been pressed yet in this round of calculations
+    if (subtractionCounter === 0 && !operationActivated) {
+      savedWorkingNumber = Number(workingNumberString);
+      previousEnteredNumber = Number(workingNumberString);
+      console.log(`Previous entered number is: ${previousEnteredNumber}`);
+      workingNumberString = "0";
+      subtractionCounter++;
+      operationActivated = true;
+    }
+    // If subtraction button has been pressed already in this round of calculations
+    else {
+      savedWorkingNumber -= Number(workingNumberString);
+      previousEnteredNumber = Number(workingNumberString);
+      console.log(`Previous entered number is: ${previousEnteredNumber}`);
+      workingNumberString = "0";
+      subtractionCounter++;
+      renderScreen(savedWorkingNumber);
+    }
+  }
+
+  // If pressed button is division button
+  if (pressedButton.classList.contains("division")) {
+    // If division hasn't been pressed yet in this round of calculations
+    if (divisionCounter === 0 && !operationActivated) {
+      savedWorkingNumber = Number(workingNumberString);
+      previousEnteredNumber = Number(workingNumberString);
+      console.log(`Previous entered number is: ${previousEnteredNumber}`);
+      console.log(`Saved working number is: ${savedWorkingNumber}`);
+      workingNumberString = "0";
+      divisionCounter++;
+      operationActivated = true;
+    }
+    // If division button has been pressed already in this round of calculations
+    else {
+      savedWorkingNumber = savedWorkingNumber / Number(workingNumberString);
+      previousEnteredNumber = Number(workingNumberString);
+      workingNumberString = "0";
+      divisionCounter++;
+      console.log(`Previous entered number is: ${previousEnteredNumber}`);
+      console.log(`Saved working number is: ${savedWorkingNumber}`);
+
+      // Check if dividing by zero
+      if (savedWorkingNumber === NaN || savedWorkingNumber === Infinity) {
+        renderScreen("Error");
+      }
+    }
+  }
+
+  // If pressed button is multiplication button
+  if (pressedButton.classList.contains("multiplication")) {
+    // If division hasn't been pressed yet in this round of calculations
+    if (multiplicationCounter === 0 && !operationActivated) {
+      savedWorkingNumber = Number(workingNumberString);
+      previousEnteredNumber = Number(workingNumberString);
+      console.log(`Previous entered number is: ${previousEnteredNumber}`);
+      console.log(`Saved working number is: ${savedWorkingNumber}`);
+      workingNumberString = "0";
+      multiplicationCounter++;
+      operationActivated = true;
+    }
+    // If multiplication button has been pressed already in this round of calculations
+    else {
+      savedWorkingNumber = savedWorkingNumber * Number(workingNumberString);
+      previousEnteredNumber = Number(workingNumberString);
+      workingNumberString = "0";
+      multiplicationCounter++;
+      console.log(`Previous entered number is: ${previousEnteredNumber}`);
+      console.log(`Saved working number is: ${savedWorkingNumber}`);
+
+      // Check if multiplying by Infinity or NaN
+      if (savedWorkingNumber === NaN || savedWorkingNumber === Infinity) {
+        renderScreen("Error");
+      }
     }
   }
 };
