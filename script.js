73bda7fe393c613ed1925f9078146ab9fd721399
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2023-06-14 15:05:47 +0100

    Added Javascript functionality
    Added in logic for when the addition button is pressed

--- script.js
+++ script.js
@@ -10,11 +10,18 @@ const selectedBorderColor = "#b18907";
 let workingNumberString = "0"; // To be updated when the user is done entering the number
 let workingNumberStringDigitCounter = 0; // To make sure that the entered number has no greater than 9 digits in total
 let decimalPointActivated = false; // To be updated when decimal point has been input
-let workingNumbersArray = []; // To compile working numbers
+let operationActivated = false;
+// let workingNumbersArray = []; // To compile working numbers
+let additionCounter = 0; // To be updated when addition button is pressed
+let subtractionCounter = 0; // To be updated when subtraction button is pressed
+let divisionCounter = 0; // To be updated when division button is pressed
+let multiplicationCounter = 0; // To be updated when multiplication button is pressed
+let savedWorkingNumber; // Number to be updated as one of the 4 main operations is pressed
+let previousEnteredNumber; // Last number to be entered
 
 // Function to update working number string by adding characters to it
-const updateWorkingNumberString = function (adder) {
-  workingNumberString += adder;
+const updateWorkingNumberString = function (adder1, adder2) {
+  workingNumberString = adder1 + adder2;
 };
 
 // Function to update the number string digit counter
@@ -22,31 +29,19 @@ const updateWorkingNumberStringDigitCounter = function () {
   workingNumberStringDigitCounter++;
 };
 
-// Function to render number on screen
+// Function to render a number on screen
 const renderScreen = function (number) {
   screen.textContent = number;
 };
 
+// Function combining updateWorkingNumberString and updateWorkingNumberStringDigitCounter
+const numberPressed = function (adder1, adder2) {
+  updateWorkingNumberString(adder1, adder2);
+  updateWorkingNumberStringDigitCounter();
+};
+
 const calculator = function (e) {
   const pressedButton = e.target; // Registering the pressed button
-
-  // If pressed button is the decimal point
-  if (pressedButton.classList.contains("decimal-point")) {
-    if (!decimalPointActivated) {
-      decimalPointActivated = true;
-      if (
-        workingNumberString.length === 1 &&
-        workingNumberString.includes("0")
-      ) {
-        updateWorkingNumberString(".");
-        updateWorkingNumberStringDigitCounter();
-        renderScreen(workingNumberString);
-      } else {
-        updateWorkingNumberString(".");
-        renderScreen(workingNumberString);
-      }
-    }
-  }
 
   // If pressed button is a number
   if (pressedButton.classList.contains("number")) {
@@ -67,8 +62,7 @@ const calculator = function (e) {
       workingNumberString.includes("0") &&
       pressedButton.dataset.number !== "0"
     ) {
-      workingNumberString = pressedButton.dataset.number;
-      updateWorkingNumberStringDigitCounter();
+      numberPressed("", pressedButton.dataset.number);
       renderScreen(workingNumberString);
     }
     // If working number is a single digit but not 0
@@ -76,17 +70,54 @@ const calculator = function (e) {
       workingNumberString.length === 1 &&
       !workingNumberString.includes("0")
     ) {
-      updateWorkingNumberString(pressedButton.dataset.number);
-      updateWorkingNumberStringDigitCounter();
+      numberPressed(workingNumberString, pressedButton.dataset.number);
       renderScreen(workingNumberString);
     }
     // The maximum number of digits a number should have is 9 digits
     else if (workingNumberStringDigitCounter < 9) {
-      updateWorkingNumberString(pressedButton.dataset.number);
-      updateWorkingNumberStringDigitCounter();
+      numberPressed(workingNumberString, pressedButton.dataset.number);
       renderScreen(workingNumberString);
     } else {
       return;
+    }
+  }
+
+  // If pressed button is the decimal point
+  if (pressedButton.classList.contains("decimal-point")) {
+    if (!decimalPointActivated) {
+      decimalPointActivated = true;
+      if (
+        workingNumberString.length === 1 &&
+        workingNumberString.includes("0")
+      ) {
+        numberPressed(workingNumberString, ".");
+        renderScreen(workingNumberString);
+      } else {
+        updateWorkingNumberString(workingNumberString, ".");
+        renderScreen(workingNumberString);
+      }
+    }
+  }
+
+  // If pressed button is addition button
+  if (pressedButton.classList.contains("addition")) {
+    // If addition hasn't been pressed yet in this round of calculations
+    if (additionCounter === 0 && !operationActivated) {
+      savedWorkingNumber = Number(workingNumberString);
+      previousEnteredNumber = Number(workingNumberString);
+      console.log(`Previous entered number is: ${previousEnteredNumber}`);
+      workingNumberString = "0";
+      additionCounter++;
+      operationActivated = true;
+    }
+    // If addition button has been pressed already
+    else {
+      savedWorkingNumber += Number(workingNumberString);
+      previousEnteredNumber = Number(workingNumberString);
+      console.log(`Previous entered number is: ${previousEnteredNumber}`);
+      workingNumberString = "0";
+      additionCounter++;
+      renderScreen(savedWorkingNumber);
     }
   }
 };
